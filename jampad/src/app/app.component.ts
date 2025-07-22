899e5bcabf8cbd87d41a544ddcaf9f50eec4f08b
chenry22 <112424609+chenry22@users.noreply.github.com>
2025-07-22 01:26:21 -0400

    more mobile friendly/compatible view

--- jampad/src/app/app.component.ts
+++ jampad/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, Routes } from '@angular/router';
 import { SidebarGroupComponent } from './sidebar-group/sidebar-group.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SidebarGroupComponent],
+  imports: [RouterOutlet, SidebarGroupComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'jampad';
+  isSidebarOpen = false;
 }
